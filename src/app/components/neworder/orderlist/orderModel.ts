f7716014f4388c09bb7eee090d1ae2fe614eb1fe
Khalilullahj2ee <atmkhalilullah91@gmail.com>
2022-01-15 13:09:51 +0600

    edit model

---
+++ src/app/components/neworder/orderlist/orderModel.ts
@@ -0,0 +1,23 @@
+export class orderModel{
+
+    trackingNum : String =  ""
+    senderName : String =  ""
+    senderEmail : String =  ""
+    senderPhone : String =  ""
+    senderAddress : String =  ""
+    docType : String =  ""
+    contents : String =  ""
+    weight : String =  ""
+    quantity : number =  0
+    amount : number =  0
+    orderDate : String =  ""
+    receiveName : String =  ""
+    receiveEmail : String =  ""
+    receiverPhon : String =  ""
+    receiverAddress : String =  ""
+    status : String =  ""
+
+    constructor() {
+    }
+   
+}
--- src/app/components/neworder/orderlist/orderlist.component.ts
+++ src/app/components/neworder/orderlist/orderlist.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { orderModel } from './orderModel';
 
 @Component({
   selector: 'app-orderlist',
@@ -8,10 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderlistComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   orderList: any
-  order: any
+  order: orderModel = new orderModel()
 
 
   ngOnInit(): void {
@@ -30,22 +32,24 @@ export class OrderlistComponent implements OnInit {
 
 
   editOrder(ords:any){
-    this.order = ords.trackingNum
+    //this.order = ords.trackingNum
     this.order.senderName = ords.senderName
-    // this.order.senderEmail = ords.senderEmail
-    // this.order.senderPhone = ords.senderPhone
-    // this.order.senderAddress = ords.senderAddress
-    // this.order.docType = ords.docType
-    // this.order.contents = ords.contents
-    // this.order.weight = ords.weight
-    // this.order.amount = ords.amount
-    // this.order.receiveName = ords.receiveName
-    // this.order.receiveEmail = ords.receiveEmail
-    // this.order.receiverPhon = ords.receiverPhon
-    // this.order.receiverAddress = ords.receiverAddress
-    // this.order.status = ords.status
-    // this.order.orderDate = ords.orderDate 
+    this.order.senderEmail = ords.senderEmail
+    this.order.senderPhone = ords.senderPhone
+    this.order.senderAddress = ords.senderAddress
+    this.order.docType = ords.docType
+    this.order.contents = ords.contents
+    this.order.weight = ords.weight
+    this.order.amount = ords.amount
+    this.order.receiveName = ords.receiveName
+    this.order.receiveEmail = ords.receiveEmail
+    this.order.receiverPhon = ords.receiverPhon
+    this.order.receiverAddress = ords.receiverAddress
+    this.order.status = ords.status
+    this.order.orderDate = ords.orderDate 
     console.log(this.order);
+
+    this.router.navigate(["/admin/order"], {state:{ords: orderModel, isSave: false}})
     
 
   }
